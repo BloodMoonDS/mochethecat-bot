795ff076c1334793393fa6f3d6cc87b7e8be158b
bloodiey <bloodmoondesignx86@gmail.com>
2023-04-10 12:47:52 -0600

    added commands as a chain

--- index.js
+++ index.js
@@ -14,45 +14,19 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 
 const ErrorMessage = "Dammint I couldn't execute that one command! Details:"
 
-
 const { REST, Routes,Discord, Client, MessageEmbed, Application, Message, RichPresenceAssets, Presence, CommandInteraction, Intents, IntentsBitField, Collection, Events,GatewayIntentBits, ClientPresence, ClientPresenceStatus} = require("discord.js");
 const axios = require('axios')
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 const client = new Client({ intents: [IntentsBitField.Flags.GuildModeration,IntentsBitField.Flags.GuildMessages ,IntentsBitField.Flags.MessageContent,IntentsBitField.Flags.GuildMessageTyping] })
 
+client.commands = new Collection();
 
 const app = express();
 // app.use(bodyParser.json());
 
 
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
-
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-// and deploy your commands!
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(APPLICATION_ID, GUILD_ID),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
-})();
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -86,6 +60,7 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
+
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
