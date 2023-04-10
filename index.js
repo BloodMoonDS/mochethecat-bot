ccab6adb34e09d9937b626c78927e67eb267a986
bloodiey <bloodmoondesignx86@gmail.com>
2023-04-10 11:11:36 -0600

    ohg

--- index.js
+++ index.js
@@ -141,15 +141,15 @@ const rest = new REST({ version: '10' }).setToken(token);
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
     
-		// The put method is used to fully refresh all commands in the guild with the current set
-		//const data = await rest.put(
-		//	Routes.applicationGuildCommands(clientId, guildId),
-		//	{ body: commands },
-	  //);
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands + { body: commands },
-    )
+	  // The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },slash_commands
+	  );
+    //let discord_response = await discord_api.put(
+    //  `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
+    //  slash_commands + { body: commands },
+    //)
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
