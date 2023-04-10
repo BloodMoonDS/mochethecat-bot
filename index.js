a5c051934f9c8264fda0c0a2e521256f3640a42a
bloodiey <bloodmoondesignx86@gmail.com>
2023-04-10 12:32:26 -0600

    i cant goive up

--- index.js
+++ index.js
@@ -24,11 +24,74 @@ const client = new Client({ intents: [IntentsBitField.Flags.GuildModeration,Inte
 const app = express();
 // app.use(bodyParser.json());
 
-app.get('/register_commands', async (req,res) =>{ // I replaced the deploy-commands.js with the register url
-  const reg = require("/modules/register.js");
-  reg.trigger()
-})
 
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(APPLICATION_ID, GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: `Maldición no pude ejecutar ese comando por favor mandale esto a bloodiey: ${error}`, ephemeral: true });
+		} else {
+			await interaction.reply({ content: `Maldición no pude ejecutar ese comando por favor mandale esto a bloodiey: ${error}`, ephemeral: true });
+		}
+	}
+});
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+app.get('/', async (req,res) =>{
+  return res.send('Follow documentation ')
+})
 
 app.listen(8999, () => {
 
